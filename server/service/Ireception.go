55578599cde4f322525991836b81bc546b648d13
ailkaya <1581201656@qq.com>
2025-02-27 13:00:05 +0800

    The project will be renamed to goulette and the version will be reset to v0.1

--- server/service/Ireception.go
+++
@@ -1,7 +0,0 @@
-package service
-
-type IReception interface {
-	Run() error
-	Ack(int32)
-	Close()
-}
--- server/service/const.go
+++
@@ -1,6 +0,0 @@
-package service
-
-const (
-	ErrorEOF = "EOF"
-	ErrorRPC = "rpc error: code = Canceled desc = context canceled"
-)
